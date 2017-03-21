f38cbfea36bb3a5fcf23c809bf75efe500954f8b
Vincent BOURDEIX <vbourdeix@gmail.com>
2017-03-21 16:41:34 +0100

    Modifications pour être plus cohérent avec la solution de Noël par rapport aux noms de variables

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cityweather');
 
 var citySchema = mongoose.Schema({
-  name: String,
+  city: String,
   description: String,
   picto: String,
-  tempMin: Number,
-  tempMax: Number
+  temp_min: Number,
+  temp_max: Number
 });
 
 var CityModel = mongoose.model('city', citySchema);
@@ -44,19 +44,12 @@ app.get('/add', function (req, res) {
     body = JSON.parse(body);
     
     console.log(req.query);
-    if(body.main.temp_min != undefined) {
-      req.query.city     = body.name;
-      req.query.temp_min = body.main.temp_min;
-      req.query.temp_max = body.main.temp_max;
-      req.query.picto    = body.weather[0].icon;
-      req.query.description = body.weather[0].description;
-      console.log(body.main.temp_min+'//'+body.main.temp_max+'//'+body.weather[0].description);
-      console.log(req.query);
-      
+    if(body.main.temp_min != undefined) {   
+
       var city = new CityModel({
-        name: body.name,
-        tempMax: body.main.temp_max,
-        tempMin: body.main.temp_min,
+        city: body.name,
+        temp_max: body.main.temp_max,
+        temp_min: body.main.temp_min,
         description: body.weather[0].description,
         picto: body.weather[0].icon
       });
