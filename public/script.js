3986b8bcb87bc2269d834b3ae4cd3dc93fb10992
Vincent BOURDEIX <vbourdeix@gmail.com>
2017-03-21 22:10:41 +0100

    Implémentation du tri et de la map

--- public/script.js
+++ public/script.js
@@ -24,6 +24,43 @@
   });
      
  });
+
+ $("#cityList").sortable();
+ $("#cityList").on( "sortupdate", function( event, ui ) {
+   console.log( $("#cityList").sortable("toArray") );
+   var data = $("#cityList").sortable("toArray");
+   $.get("/sort", { sortData: data });
+ });
+ 
+  
+  var map = L.map('mapid');
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+ 
+  
+  var lon;
+  var lat;
+  $("li").each(function(index) {
+    
+    if(index == 0){
+      lon = $(this).data("lon");
+      lat = $(this).data("lat");
+    }
+    
+    var tempMin = $(this).find(".temp-min").text();
+    var tempMax = $(this).find(".temp-max").text();
+    L.marker([$(this).data("lat"), $(this).data("lon")]).addTo(map).bindPopup("temp min: "+tempMin+" | temp max: "+tempMax);
+  });
+    
+  map.setView([lat, lon], 10)  
+    
+
+  $("li").click(function(){
+    lat = $(this).data("lat");
+    lon = $(this).data("lon");
+    map.setView([lat, lon], 10);    
+  });
  
 })
 
--- server.js
+++ server.js
@@ -9,22 +9,28 @@ var citySchema = mongoose.Schema({
   description: String,
   picto: String,
   temp_min: Number,
-  temp_max: Number
+  temp_max: Number,
+  sortOrder: Number,
+  lat: Number,
+  lon: Number
 });
 
 var CityModel = mongoose.model('city', citySchema);
 var cityList;
+CityModel.find(null, null, {sort: {sortOrder : 1}}, function(error, cities) {
+  if (error) {
+    console.log("Erreur de récupération des villes");
+    return;
+  }
+  cityList = cities;
+});
 
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
-
-
-
 app.get('/', function (req, res) {
-  CityModel.find(function(error, cities) {
+  CityModel.find(null, null, {sort: {sortOrder : 1}}, function(error, cities) {
     if (error) {
       console.log("Erreur de récupération des villes");
       return;
@@ -44,22 +50,31 @@ app.get('/add', function (req, res) {
     body = JSON.parse(body);
     
     console.log(req.query);
-    if(body.main.temp_min != undefined) {   
+    if(body.main.temp_min != undefined) {  
+
+      // Initialisation de la propriété "sortOrder" à 0
+      var newCitySortOrder = 0;
+      //Si au moins 1 element dans le tableau, recuperer la propriété "sortOrder" du dernier élément et lui ajouter +1
+      if(cityList.length > 0) {
+         newCitySortOrder = cityList[cityList.length-1].sortOrder + 1;
+      }
 
       var city = new CityModel({
-        city: body.name,
-        temp_max: body.main.temp_max,
-        temp_min: body.main.temp_min,
+        city:        body.name,
+        temp_max:    body.main.temp_max,
+        temp_min:    body.main.temp_min,
         description: body.weather[0].description,
-        picto: body.weather[0].icon
+        picto:       body.weather[0].icon,
+        sortOrder:   newCitySortOrder,
+        lat:         body.coord.lat,
+        lon:         body.coord.lon
+
       });
 
       city.save(function (err, city) {
         if (err) return console.error(err);
         cityList.push(city);
-        res.render('meteo', {
-          list : cityList
-        });
+        res.redirect('/');
       });
     }
   
@@ -81,10 +96,26 @@ app.get('/delete', function (req, res) {
     }
     cityList.splice(req.query.indice, 1);
 
-    res.render('meteo', {
-      list : cityList
-    });
+    res.redirect('/');
   });
+});
+
+app.get('/sort', function (req, res) {
+  var index;
+  var cityListTmp = [];
+  
+  var sortedIndexList = req.query.sortData;
+
+  for(var i=0; i<sortedIndexList.length; i++) {
+    initialIndex = sortedIndexList[i];
+    console.log(cityList, initialIndex);
+    currentCity = cityList[initialIndex];
+    cityListTmp.push(currentCity);
+    // Mise à jour de la propriété "sortOrder" ayant comme valeur l'index de la boucle (qui va en ordre croissant)
+    console.log('update de la ville'+currentCity.id, i);
+    CityModel.update({ _id: currentCity.id}, { sortOrder : i}, function(error, raw) {});
+  }
+  cityList = cityListTmp;
 });
 
 app.listen(80, function () {
